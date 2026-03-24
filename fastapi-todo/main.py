cc23305d36f26791e4636fddc26f69f5b2e3e711
Gajapathi K <113296010+ViratGaja@users.noreply.github.com>
2026-03-24 15:46:24 +0530

    learn

---
+++ fastapi-todo/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Depends
+from schemas import Todo as TodoSchema, TodoCreate
+from database import SessionLocal, Base, engine
+from sqlalchemy.orm import Session
+from models import Todo
+
+# Database-la intha table illana, thonthira illama create pannidum
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# Database Connection handle panna intha function (Dependency)
+def get_db():
+    db = SessionLocal() # Connection-ah open pannuthu
+    try:
+        yield db       # Intha connection-ah API function-ku tharuval (Provide)
+    finally:
+        db.close()     # Velai mudinja apram connection-ah close pannidum (Safety)
+
+# POST Method - Pudhu Todo-va create panna
+@app.post("/todos", response_model=TodoSchema)
+def create(todo: TodoCreate, db: Session = Depends(get_db)):
+     
+     # Pydantic Schema data-va (JSON) SQL Model-ah mathuthu
+     # **todo.dict() na title, description ella data-vaiyum "unpack" pannum
+     db_todo = Todo(**todo.dict()) 
+     
+     db.add(db_todo)      # Intha data-va Database-la 'Add' pannu (Stage)
+     db.commit()          # Changes-ah database-la 'Save' pannu (Finalize)
+     db.refresh(db_todo)  # DB-la generate aana 'id'-ya thirumba model-ku kondu vaa
+     
+     return db_todo       # Create aana data-va response-ah thirumba anupu
---
+++ fastapi-todo/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+class TodoBase(BaseModel):
+    title: str                      # Inga 'title' kandippa string-ah irukanum (Required)
+    description: str | None = None  # Idhu optional, illana 'None' (null) nu eduthukkum
+    completed: bool = False         # Default-ah false-nu set aagidum (Boolean type)
+
+# create pannum pothu use aagum
+class TodoCreate(TodoBase):
+    pass                             # TodoBase-la irukura ellathaiyum அப்படியே copy pannikkum (Inheritance)
+
+# Database-la irunthu response anupum pothu use aagum
+class Todo(TodoBase):
+    id: int                         # Database create panna 'id' inga add aagum
+
+    class Config:                   # Pydantic-oda special settings idhu
+        orm_mode = True             # SQLAlchemy (ORM) objects-ah direct-ah JSON-ah mathurathuku idhu romba mukkiyam
